614422fb9ede5457fd5a2b139d88e98efc7a22cb
Inaayat <125380942+Inaay@users.noreply.github.com>
2023-03-14 21:50:33 +0400

    Add files via upload

---
+++ src/Inaayat/Killstreak/EventListener.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Inaayat\Killstreak;
+
+use pocketmine\console\ConsoleCommandSender;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\player\Player;
+
+class EventListener implements Listener {
+
+	public $plugin;
+
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+    }
+
+	public function onDeath(PlayerDeathEvent $event) {
+		$player = $event->getPlayer();
+		$killer = $player->getLastDamageCause()->getDamager();
+		if ($killer instanceof Player && $player instanceof Player) {
+			$killerName = $killer->getName();
+			$this->plugin->resetKillstreak($player);
+			$this->plugin->increaseKillstreak($killer);
+			$killStreak = $this->plugin->getKillStreak($killer);
+			$config = $this->plugin->getConfig()->get("killstreak");
+			if (isset($config[$killStreak])) {
+				$command = str_replace("{player}", $killerName, $config[$killStreak]["command"]);
+				$command = str_replace("{milestone}", $config[$killStreak]["milestone"], $command);
+				$this->plugin->getServer()->dispatchCommand(new ConsoleCommandSender($this->plugin->getServer(), $this->plugin->getServer()->getLanguage()), $command);
+				if ($this->plugin->getConfig()->get("Broadcast")) {
+					$broadcastMessage = str_replace("{player}", $killerName, $this->plugin->getConfig()->get("BroadcastMessage"));
+					$broadcastMessage = str_replace("{milestone}", $config[$killStreak]["milestone"], $broadcastMessage);
+					$broadcastMessage = str_replace("&", "§", $broadcastMessage);
+					$this->plugin->getServer()->broadcastMessage($this->plugin->getPrefix() . $broadcastMessage);
+				}
+			}
+		}
+	}
+}
---
+++ src/Inaayat/Killstreak/Main.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Inaayat\Killstreak;
+
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase {
+
+	private $killstreaks = [];
+
+    public function onEnable(): void {
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    }
+
+	public function getPrefix(): string {
+		return "§7[§cKillstreak§7]§r ";
+	}
+
+	public function increaseKillstreak(Player $player): void {
+        $uuid = $player->getUniqueId()->toString();
+        if (!isset($this->killstreaks[$uuid])) {
+            $this->killstreaks[$uuid] = 1;
+        } else {
+            $this->killstreaks[$uuid]++;
+        }
+    }
+    
+    public function getKillStreak(Player $player): int {
+        $uuid = $player->getUniqueId()->toString();
+        return isset($this->killstreaks[$uuid]) ? $this->killstreaks[$uuid] : 0;
+    }
+
+	public function resetKillstreak(Player $player): void {
+		$uuid = $player->getUniqueId()->toString();
+		unset($this->killstreaks[$uuid]);
+	}
+}
