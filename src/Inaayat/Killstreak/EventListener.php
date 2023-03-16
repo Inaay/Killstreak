4d1baa3eb9176e9d2cb54b78ca359c409c22f698
Inaayat <125380942+Inaay@users.noreply.github.com>
2023-03-16 17:26:14 +0400

    if $cause instanceof EntityDamageByEntityEvent

--- src/Inaayat/Killstreak/EventListener.php
+++ src/Inaayat/Killstreak/EventListener.php
@@ -2,7 +2,9 @@
 
 namespace Inaayat\Killstreak;
 
+use Inaayat\Core\Main;
 use pocketmine\console\ConsoleCommandSender;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\player\Player;
@@ -14,25 +16,28 @@ class EventListener implements Listener {
     public function __construct(Main $plugin){
         $this->plugin = $plugin;
     }
-
+	
 	public function onDeath(PlayerDeathEvent $event) {
 		$player = $event->getPlayer();
-		$killer = $player->getLastDamageCause()->getDamager();
-		if ($killer instanceof Player && $player instanceof Player) {
-			$killerName = $killer->getName();
-			$this->plugin->resetKillstreak($player);
-			$this->plugin->increaseKillstreak($killer);
-			$killStreak = $this->plugin->getKillStreak($killer);
-			$config = $this->plugin->getConfig()->get("killstreak");
-			if (isset($config[$killStreak])) {
-				$command = str_replace("{player}", $killerName, $config[$killStreak]["command"]);
-				$command = str_replace("{milestone}", $config[$killStreak]["milestone"], $command);
-				$this->plugin->getServer()->dispatchCommand(new ConsoleCommandSender($this->plugin->getServer(), $this->plugin->getServer()->getLanguage()), $command);
-				if ($this->plugin->getConfig()->get("Broadcast")) {
-					$broadcastMessage = str_replace("{player}", $killerName, $this->plugin->getConfig()->get("BroadcastMessage"));
-					$broadcastMessage = str_replace("{milestone}", $config[$killStreak]["milestone"], $broadcastMessage);
-					$broadcastMessage = str_replace("&", "§", $broadcastMessage);
-					$this->plugin->getServer()->broadcastMessage($this->plugin->getPrefix() . $broadcastMessage);
+		$cause = $player->getLastDamageCause();
+		if ($cause instanceof EntityDamageByEntityEvent) {
+			$killer = $cause->getDamager();
+			if ($killer instanceof Player && $player instanceof Player) {
+				$killerName = $killer->getName();
+				$this->plugin->resetKillstreak($player);
+				$this->plugin->increaseKillstreak($killer);
+				$killStreak = $this->plugin->getKillStreak($killer);
+				$config = $this->plugin->getConfig()->get("killstreak");
+				if (isset($config[$killStreak])) {
+					$command = str_replace("{player}", $killerName, $config[$killStreak]["command"]);
+					$command = str_replace("{milestone}", $config[$killStreak]["milestone"], $command);
+					$this->plugin->getServer()->dispatchCommand(new ConsoleCommandSender($this->plugin->getServer(), $this->plugin->getServer()->getLanguage()), $command);
+					if ($this->plugin->getConfig()->get("Broadcast")) {
+						$broadcastMessage = str_replace("{player}", $killerName, $this->plugin->getConfig()->get("BroadcastMessage"));
+						$broadcastMessage = str_replace("{milestone}", $config[$killStreak]["milestone"], $broadcastMessage);
+						$broadcastMessage = str_replace("&", "§", $broadcastMessage);
+						$this->plugin->getServer()->broadcastMessage($this->plugin->getPrefix() . $broadcastMessage);
+					}
 				}
 			}
 		}
